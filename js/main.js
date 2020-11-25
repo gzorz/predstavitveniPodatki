0f7718e3f03d150cd6eca425b3086b0a9d3c0382
gzorz <gregazorz@gmail.com>
2020-11-25 20:10:19 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,166 @@
+    //privzeto
+    function changeOzadje1(color) {
+        document.getElementById("PanelMK").style.color = "black";
+        document.getElementById("vrstica").style.color = "#397289";
+        document.getElementById("dost-nasl").style.color = "black";
+        document.getElementById("dost-nasl1").style.color = "black";
+        document.body.style.background = '#FFFFFF';
+        document.getElementById("PanelMK").style.background = "#FFFFFF";
+        document.getElementById("logo_bel").src = "slike/RS_gov_si-logo.png";
+    }
+    //IZPUSTI
+    function changeOzadje2() {
+        document.getElementById("PanelMK").style.color = "white";
+        document.getElementById("vrstica").style.color = "white";
+        document.getElementById("dost-nasl").style.color = "white";
+        document.getElementById("dost-nasl1").style.color = "white";
+        document.body.style.background = "#CD5C5C";
+        document.getElementById("PanelMK").style.background = "#CD5C5C";
+        document.getElementById("logo_bel").src = "slike/RS_gov_si-logo_bel.png";
+    }
+    function changeOzadje3(color) {
+        document.getElementById("PanelMK").style.color = "black";
+        document.getElementById("vrstica").style.color = "black";
+        document.getElementById("dost-nasl").style.color = "black";
+        document.getElementById("dost-nasl1").style.color = "black";
+        document.body.style.background = "#f5f5dc";
+        document.getElementById("PanelMK").style.background = "#f5f5dc";
+        document.getElementById("logo_bel").src = "slike/RS_gov_si-logo.png";
+    }
+    //rumeno na modrem
+    function changeOzadje4(color) {
+        document.getElementById("PanelMK").style.color = "#00008b";
+        document.getElementById("vrstica").style.color = "#00008b";
+        document.getElementById("dost-nasl").style.color = "#00008b";
+        document.getElementById("dost-nasl1").style.color = "#00008b";
+        document.body.style.background = "white";
+        document.getElementById("PanelMK").style.background = "white";
+        document.getElementById("logo_bel").src = "slike/RS_gov_si-logo.png";
+    }
+    //rumeno na črni
+    function changeOzadje5(color) {
+        document.getElementById("PanelMK").style.color = "black";
+        document.getElementById("vrstica").style.color = "black";
+        document.getElementById("dost-nasl").style.color = "black";
+        document.getElementById("dost-nasl1").style.color = "black";
+        document.body.style.background = "#00ff00";
+        document.getElementById("PanelMK").style.background = "#00ff00";
+        document.getElementById("logo_bel").src = "slike/RS_gov_si-logo.png";
+    }
+    //modro na rumenem
+    function changeOzadje6(color) {
+        document.getElementById("PanelMK").style.color = "black";
+        document.getElementById("vrstica").style.color = "black";
+        document.getElementById("dost-nasl").style.color = "black";
+        document.getElementById("dost-nasl1").style.color = "black";
+        document.body.style.background = "#ffe800";
+        document.getElementById("PanelMK").style.background = "#ffe800";
+        document.getElementById("logo_bel").src = "slike/RS_gov_si-logo.png";
+    }
+    //modro na rumenem
+    function changeOzadje7(color) {
+        document.getElementById("PanelMK").style.color = "#ffe800";
+        document.getElementById("vrstica").style.color = "#ffe800";
+        document.getElementById("dost-nasl").style.color = "#ffe800";
+        document.getElementById("dost-nasl1").style.color = "#ffe800";
+        document.body.style.background = "#00008b";
+        document.getElementById("PanelMK").style.background = "#00008b";
+        document.getElementById("logo_bel").src = "slike/RS_gov_si-logo_bel.png";
+    }
+    //modro na rumenem
+    function changeOzadje8(color) {
+        document.getElementById("PanelMK").style.color = "#00f0ff";
+        document.getElementById("vrstica").style.color = "#00f0ff";
+        document.getElementById("dost-nasl").style.color = "#00f0ff";
+        document.getElementById("dost-nasl1").style.color = "#00f0ff";
+        document.body.style.background = "black";
+        document.getElementById("PanelMK").style.background = "black";
+        document.getElementById("logo_bel").src = "slike/RS_gov_si-logo_bel.png";
+    }
+    //modro na rumenem
+    function changeOzadje9(color) {
+        document.getElementById("PanelMK").style.color = "#00008b";
+        document.getElementById("vrstica").style.color = "#00008b";
+        document.getElementById("dost-nasl").style.color = "#00008b";
+        document.getElementById("dost-nasl1").style.color = "#00008b";
+        document.body.style.background = "#ffe800";
+        document.getElementById("PanelMK").style.background = "#ffe800";
+        document.getElementById("logo_bel").src = "slike/RS_gov_si-logo.png";
+    }
+    //modro na rumenem
+    function changeOzadje10(color) {
+        document.getElementById("PanelMK").style.color = "black";
+        document.getElementById("vrstica").style.color = "black";
+        document.getElementById("dost-nasl").style.color = "black";
+        document.getElementById("dost-nasl1").style.color = "black";
+        document.body.style.background = "#d600d6";
+        document.getElementById("PanelMK").style.background = "#d600d6";
+        document.getElementById("logo_bel").src = "slike/RS_gov_si-logo.png";
+    }
+
+    //skrij in pokazi
+    function PrikaziDostop() {
+        var x = document.getElementById("myDIV");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+
+    //pisava1 - privzeto
+    function changePisavo0() {
+        document.getElementById("PanelMK").style.fontFamily = "'republika";
+        document.getElementById("vrstica").style.fontFamily = "republika";
+        document.getElementById("dost-nasl").style.fontFamily = "republika";
+        document.getElementById("dost-nasl1").style.fontFamily = "republika";
+        document.getElementById("PanelMK").style.fontWeight = "300";
+    }
+
+    function changePisavo1() {
+        document.getElementById("PanelMK").style.fontFamily = "Arial";
+        document.getElementById("vrstica").style.fontFamily = "Arial";
+        document.getElementById("dost-nasl").style.fontFamily = "Arial";
+        document.getElementById("dost-nasl1").style.fontFamily = "Arial";
+        document.getElementById("PanelMK").style.fontWeight = "300";
+    }
+    //pisava2
+    function changePisavo2() {
+        document.getElementById("PanelMK").style.fontFamily = "Arial";
+        document.getElementById("vrstica").style.fontFamily = "Arial";
+        document.getElementById("dost-nasl").style.fontFamily = "Arial";
+        document.getElementById("dost-nasl1").style.fontFamily = "Arial";
+        document.getElementById("PanelMK").style.fontWeight = "900";
+        document.getElementById("vrstica").style.fontWeight = "900";
+        document.getElementById("dost-nasl").style.fontWeight = "900";
+        document.getElementById("dost-nasl1").style.fontWeight = "900";
+        document.getElementById("tbxIme").style.fontWeight = "900";
+        document.getElementById("tbxopis").style.fontWeight = "900";
+        document.getElementById("tbxavtor").style.fontWeight = "900";
+        document.getElementById("tbxZbirka").style.fontWeight = "900";
+        document.getElementById("tbxInstitucija").style.fontWeight = "900";
+        document.getElementById("tbxDediscina").style.fontWeight = "900";
+        document.getElementById("tbxesd").style.fontWeight = "900";
+        document.getElementById("tbxIDGlobalni").style.fontWeight = "900";
+        document.getElementById("tbxIDLokalni").style.fontWeight = "900";
+        document.getElementById("Europeana").style.fontWeight = "900";
+    }
+    //pisava3
+    function changePisavo3() {
+        document.getElementById("PanelMK").style.fontFamily = "Verdana";
+        document.getElementById("vrstica").style.fontFamily = "Verdana";
+        document.getElementById("dost-nasl").style.fontFamily = "Verdana";
+        document.getElementById("dost-nasl1").style.fontFamily = "Verdana";
+        document.getElementById("PanelMK").style.fontWeight = "300";
+    }
+    //pisava4
+    function changePisavo4() {
+        document.getElementById("PanelMK").style.fontFamily = "gothic";
+        document.getElementById("vrstica").style.fontFamily = "gothic";
+        document.getElementById("dost-nasl").style.fontFamily = "gothic";
+        document.getElementById("dost-nasl1").style.fontFamily = "gothic";
+        document.getElementById("PanelMK").style.fontWeight = "300";
+
+
+
+    }
